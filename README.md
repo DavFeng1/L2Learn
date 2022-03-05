83722a60496ea77c7d19b7876f6ac9512170d2bb
David Feng <fenghourun@gmail.com>
2022-03-05 15:34:48 -0500

    Add MNIST dataset

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # L2Learn 🧠🖥️
 
-Zero dependency implementations of various machine learning models
+Zero dependency implementations of various machine learning models.
+
+Includes a mathematical derivation of each the models
 
 - Neural Network
--- src/neuralNet.py
+++ src/neuralNet.py
@@ -2,8 +2,25 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plot
 
-def neuralNet():
-  print("hello world")
+def loadData():
+
+  testData = pd.read_csv("./datasets/mnist/mnistTest.csv")
+
+  print("Loaded test data")
+
+  trainData = pd.read_csv("./datasets/mnist/mnistTrain.csv")
+
+  print("Loaded training data")
+
+  test = np.array(testData)
+
+  print("Test data shape")
+  print(test.shape)
+
+  test2 = np.array([[[1], [2], [3]]])
 
 
-neuralNet()
+
+
+
+
