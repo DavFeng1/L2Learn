Fix weights calculation

src/backProp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 from matplotlib import pyplot as plt
 np.seterr(all='raise')
 
@@ -8,21 +9,19 @@ trainDataFrame = pd.read_csv("./datasets/mnist/mnistTrain.csv")
 testData = np.array(testDataFrame)
 trainData = np.array(trainDataFrame)
 
+# Normalize data
 epsilon = 0.001
-Y = np.array([[1 if label == i else 0 for i in range(10)] for label in trainData[:,0]])
+Y = np.array([[1 if label == i else 0 for i in range(10)] 
+  for label in trainData[:,0]])
+
 X = trainData[:,1:] # The input vector
+X = X / X.sum(axis=1)[:, np.newaxis] # Normalize on a per row basis
 n = 784 # Dimension of the input space (28 * 28 = 784 pixels)
 m = 4 # Dimension of hidden layer ( 4 nodes )
 l = 10 # Dimension of output ( 10 possible digits to classify )
 k = 60000 # Size of training data
 
 # Define functions
-def sigmoid(x):
-  return 1 / ( 1 + np.exp(-x))
-
-def sigmoidPrime(x):
-  return np.exp(-x) / ( 1 + np.exp(-x)) ** 2
-
 def ReLU(Z):
   return np.maximum(Z, 0)
 
@@ -30,11 +29,8 @@ def ReluPrime(Z):
   return Z > 0
 
 def softmax(Z):
-  try:
-    A = np.exp(Z) / sum(np.exp(Z))
-    return A
-  except FloatingPointError:
-    print("Floating point error", Z)
+  A = np.exp(Z) / sum(np.exp(Z))
+  return A
 
 def error(l, p):
   if (len(p) != len(l)):
@@ -53,41 +49,37 @@ def initializeParameters():
   return W0, b0, W1, b1
 
 def forwardProp(W0, W1, b0, b1, X_i):
-  # Z is value before activation function is applied
   A0 = W0.T.dot(X_i) + b0
   Z0 = ReLU(A0)
   Z1 = W1.T.dot(Z0) + b1
   P = softmax(Z1)
+
+  if not math.isclose(np.sum(P), 1.0):
+    raise ValueError("forwardProp: P does not sum to 1")
+
   return A0, P
 
 def backProp(W1, P, X_i, Y_i, A0):
 
   def z(k):
     return (P[k] - Y_i[k]) * P[k] * (1 + P[k])
-
-  print("A0================================", A0)
-  print("X_i================================", X_i)
-  print("P================================", np.sum(P))
-  print("Y_i================================", Y_i)
-
-  dW0 = (1 / n ) * np.array([[ReluPrime(A0[j]) 
+  
+  dW0 = np.array([[ReluPrime(A0[j]) 
     * X_i[j] 
-    * np.sum([z(k) * W1[j][k] for k in range(l)]) 
-        for i in range(n)] 
+    * np.sum([z(k) * W1[j][k] for k in range(l)]) for i in range(n)] 
           for j in range(m)])
 
-  db0 =  ( 1/ n ) * np.array([
+  db0 = np.array([
     ReluPrime(A0[i]) * 
     np.sum([z(k) * W1[i][k] for k in range(l)]) 
       for i in range(m)])
 
-  dW1 = (1 / n ) * np.array([[ReluPrime(A0[i]) 
+  dW1 =  np.array([[ReLU(A0[i]) 
     * z(j) 
-    * A0[i] 
       for i in range(m)] 
         for j in range(l)])
 
-  db1 =  ( 1 / n ) * np.array([z(k) for k in range(l)])
+  db1 =  np.array([z(k) for k in range(l)])
 
   return dW0, db0, dW1, db1
 
@@ -109,17 +101,24 @@ def updateParams(W0, W1, b0, b1, dW0, dW1, db0, db1, epsilon):
 def gradientDescent(X, Y, epsilon, iterations):
   
   W0, b0, W1, b1 = initializeParameters()
-
+  errors = []
   for i in range(iterations):
     A0, P = forwardProp(W0, W1, b0, b1, X[i])
     dW0, db0, dW1, db1 = backProp(W1, P, X[i], Y[i], A0)
     W0, b0, W1, b1 = updateParams(W0, W1, b0, b1, dW0, dW1, db0, db1, epsilon)
 
-    print("Iteration: ===================", i)
-    e = error(P, Y[i])
-    print("error", e)
+    currError = error(P, Y[i])
+    errors.append(currError)
 
+    if i % 50 == 0:
+      print("Iteration: ===================", i)
+      print("Error: ", currError)
+
+  plt.plot(errors)
+  plt.ylabel("Errors")
+  plt.savefig('errors.png')
   return W0, b0, W1, b1
 
 
-W0, b0, W1, b1 = gradientDescent( X, Y, epsilon, 50)
+W0, b0, W1, b1 = gradientDescent( X, Y, epsilon, 2)
+# %%